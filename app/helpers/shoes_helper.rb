bee8a3a7ac55ce853d01625756ab08880acce98b
Mateo <mateo.ochoac@gmail.com>
2019-06-25 16:57:52 -0400

    Disable buttons if the item has already been paid for

--- app/helpers/shoes_helper.rb
+++ app/helpers/shoes_helper.rb
@@ -1,2 +1,31 @@
 module ShoesHelper
+  def paid_for_checkbox(f, shoe)
+    if payment_options_disabled?(shoe)
+      f.check_box :paid_for, disabled: "disabled"
+    else
+      f.check_box :paid_for
+    end
+  end
+
+  def payment_options_button(f, shoe, payment_type)
+    if payment_options_disabled?(shoe)
+      f.radio_button :type_of_payment, payment_type, disabled: "disabled"
+    else
+      f.radio_button :type_of_payment, payment_type
+    end
+  end
+
+  def cost_input_field(f, shoe)
+    if payment_options_disabled?(shoe)
+      f.text_field :cost, disabled: "disabled"
+    else
+      f.text_field :cost
+    end
+  end
+
+  private
+
+  def payment_options_disabled?(shoe)
+    Shoe.exists?(shoe.id) && shoe.paid_for?
+  end
 end
