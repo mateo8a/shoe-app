b4679066505228782e2437140c5b515193342482
Mateo <mateo.ochoac@gmail.com>
2019-06-25 15:58:58 -0400

    Interactive button to pick type of payment
    JS to prevent invalid data

---
+++ app/assets/javascripts/shoes.js
@@ -0,0 +1,17 @@
+/*# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/*/
+
+$(document).ready(function() {
+  $("#paid-for-field").on("click", function(event) {
+    event.stopPropagation();
+    if ($('#shoe_paid_for').prop("checked")) {
+      $("#type-of-payment.hidden").removeClass("hidden");
+    } else {
+      $("#type-of-payment").addClass("hidden");
+      // remove checked from both payment options
+      $("#shoe_type_of_payment_card").prop( "checked", false );
+      $("#shoe_type_of_payment_cash").prop( "checked", false );
+    };
+  });
+});
