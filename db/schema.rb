0bccbfb149898d13ce76c90923aba8920a4fc4c4
Mateo <mateo.ochoac@gmail.com>
2019-07-10 15:12:27 -0400

    Run db:migrate after creation of scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_165911) do
+ActiveRecord::Schema.define(version: 2019_07_10_190540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shoes", force: :cascade do |t|
     t.string "color"
