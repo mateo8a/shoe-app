59f523d66313735d2c7b3648613e41ad349fc856
Mateo <mateo.ochoac@gmail.com>
2019-08-05 22:32:08 -0400

    Add configuration for Mailer so devise can send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Added per active admin install instructions
+  config.action_mailer.default_url_options = { :host => 'zapateria-ochoa.com' }
+
+  # These settings are for the sending out email
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+  {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', # you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV["SHOE_APP_EMAIL"],
+    :password           => ENV["SHOE_APP_EMAIL_PASSWORD"],
+    :authentication     => "plain"
+  }
 end
